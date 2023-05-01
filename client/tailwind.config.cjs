f52dabbb0ba20557147593bf4d1b91de80206d8c
Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2023-05-01 22:42:44 +0530

    Added OpenAI like login

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -29,24 +29,36 @@ module.exports = {
       },
       colors: {
         gray: {
-          '50': '#f7f7f8',
-          '100': '#d9d9e3',
-          '200': '#d9d9e3', // Replacing .bg-gray-200
-          '300': '#c5c5d2',
-          '400': '#acacb1',
-          '500': '#8e8ea0',
-          '600': '#565869',
-          '700': '#40414f', // Replacing .dark .dark:bg-gray-700 and .dark .dark:hover:bg-gray-700:hover
-          '800': '#343541', // Replacing .dark .dark:bg-gray-800, .bg-gray-800, and .dark .dark:hover:bg-gray-800\/90
-          '900': '#202123' // Replacing .dark .dark:bg-gray-900, .bg-gray-900, and .dark .dark:hover:bg-gray-900:hover
-        }
+          50: "#f5f7fa",
+          100: "#e9eff6",
+          200: "#c8d8e9",
+          300: "#a7c0dc",
+          400: "#6f9ed0",
+          500: "#1d7fd5",
+          600: "#1565a8",
+          700: "#0f4b7a",
+          800: "#0a3b5c",
+          900: "#08304b",
+          1000: "#08304b",
+        },
+				 green: {
+          50: "#f1f9f7",
+          100: "#def2ed",
+          200: "#a6e5d6",
+          300: "#6dc8b9",
+          400: "#41a79d",
+          500: "#1d8c83",
+          600: "#126e6b",
+          700: "#0a4f53",
+          800: "#06373e",
+          900: "#031f29",
+        },
     }
     }
   },
   plugins: [
     require('tailwindcss-animate'),
     require("tailwindcss-radix")(),
-    require('@tailwindcss/forms'),
     // require('@tailwindcss/typography'),
   ]
 };
